1a60b309fbbe43a27471b9072c433ccfddfc51d7
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2022-07-05 07:54:54 +0200

    advent-of-code day 3 part 2

--- src/test/kotlin/de/gma/gamma/adventOfCode/AdventOfCodeTest.kt
+++ src/test/kotlin/de/gma/gamma/adventOfCode/AdventOfCodeTest.kt
@@ -18,6 +18,7 @@ class AdventOfCodeTest : BaseEvaluationTest() {
             "day2_1.gma,1507611",
             "day2_2.gma,1880593125",
             "day3_1.gma,4138664",
+            "day3_2.gma,4273224",
             "day4_1.gma,8442",
             "day4_2.gma,4590",
         ]
