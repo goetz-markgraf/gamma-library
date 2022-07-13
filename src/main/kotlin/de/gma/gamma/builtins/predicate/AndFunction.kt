144885ad978fb370349e74451fd9005da2fa9546
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2022-07-13 17:06:46 +0200

    And and Or return early

--- src/main/kotlin/de/gma/gamma/builtins/predicate/AndFunction.kt
+++ src/main/kotlin/de/gma/gamma/builtins/predicate/AndFunction.kt
@@ -8,6 +8,10 @@ import de.gma.gamma.datatypes.values.BooleanValue
 object AndFunction : BuiltinFunction(listOf("a", "b")) {
     override fun callInternal(scope: Scope, callParams: List<Value>): Value {
         val a = callParams[0].evaluate(scope).toBoolean()
+
+        if (!a.boolValue)
+            return BooleanValue.build(false)
+
         val b = callParams[1].evaluate(scope).toBoolean()
 
         return BooleanValue.build(a.boolValue && b.boolValue)
