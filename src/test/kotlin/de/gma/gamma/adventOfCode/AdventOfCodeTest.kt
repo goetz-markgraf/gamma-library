ad91c8acba9d2e77fdd6c34348d7b5d24c43ecf5
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2022-07-14 16:41:22 +0200

    advent-of-code day 7 part 2

--- src/test/kotlin/de/gma/gamma/adventOfCode/AdventOfCodeTest.kt
+++ src/test/kotlin/de/gma/gamma/adventOfCode/AdventOfCodeTest.kt
@@ -25,7 +25,8 @@ class AdventOfCodeTest : BaseEvaluationTest() {
             "day5_2.gma,21140",
             "day6_1.gma,365131",
             "day6_2.gma,1650309278600",
-            "day7_1.gma,342534"
+            "day7_1.gma,342534",
+            "day7_2.gma,94004208"
         ]
     )
     fun `check advent-of-code`(filename: String, expected: Long) {
