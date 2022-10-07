74fad5ca1632c4c1e46bb6baed71e6917ba33a2e
Goetz Markgraf <goeetz.markgraf@codecentric.de>
2022-10-07 17:08:15 +0200

    added some tests for string

--- src/test/kotlin/de/gma/gamma/evaluation/string/BasicStringTest.kt
+++ src/test/kotlin/de/gma/gamma/evaluation/string/BasicStringTest.kt
@@ -1,8 +1,9 @@
 package de.gma.gamma.evaluation.string
 
 import de.gma.gamma.datatypes.StringValue
+import de.gma.gamma.datatypes.values.IntegerValue
 import de.gma.gamma.evaluation.BaseEvaluationTest
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class BasicStringTest : BaseEvaluationTest() {
@@ -10,7 +11,41 @@ class BasicStringTest : BaseEvaluationTest() {
     fun `access last element of empty string`() {
         val expr = execute("last \"\"") as StringValue
 
-        Assertions.assertThat(expr.strValue).isEqualTo("")
+        assertThat(expr.strValue).isEqualTo("")
     }
 
+    @Test
+    fun `calculate the length of the string`() {
+        val result = execute("size \"abc\"") as IntegerValue
+
+        assertThat(result.longValue).isEqualTo(3L)
+    }
+
+    @Test
+    fun `access first character`() {
+        val result = execute("first \"abc\"") as StringValue
+
+        assertThat(result.strValue).isEqualTo("a")
+    }
+
+    @Test
+    fun `access last character`() {
+        val result = execute("last \"abc\"") as StringValue
+
+        assertThat(result.strValue).isEqualTo("c")
+    }
+
+    @Test
+    fun `access the tail of a string`() {
+        val result = execute("tail \"abc\"") as StringValue
+
+        assertThat(result.strValue).isEqualTo("bc")
+    }
+
+    @Test
+    fun `access a slice of the string`() {
+        val result = execute("slice 1 2 \"abc\"") as StringValue
+
+        assertThat(result.strValue).isEqualTo("bc")
+    }
 }
